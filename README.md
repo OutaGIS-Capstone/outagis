1082e47b213151da11f13b0959ef27fb8317d912
Natalie Balashov <margaret.balashov@gmail.com>
2025-01-07 18:08:10 -0800

    Add home page layout from Figma mock-up
    * Add an Open Streets Map iframe as placeholder for the ArcGIS map
    * Separate the outage map and outage reporting tool as separate tabs
    * Mess around with the CSS (likely needs fixing)

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-# outagis
-Primary repository to store all source code and related testing material, information, and documentation for the UBC 2024W Capstone project HA-30: OutaGIS
-=======
-## AWS Amplify React+Vite Starter Template
+# OutaGIS
 
-This repository provides a starter template for creating applications using React+Vite and AWS Amplify, emphasizing easy setup for authentication, API, and database capabilities.
+Primary repository to store all source code and related testing material, information, and documentation for the UBC 2024W Capstone project HA-30: OutaGIS
 
 ## Overview
 
-This template equips you with a foundational React application integrated with AWS Amplify, streamlined for scalability and performance. It is ideal for developers looking to jumpstart their project with pre-configured AWS services like Cognito, AppSync, and DynamoDB.
+This repository was created via a starter template for creating applications using React+Vite and AWS Amplify, emphasizing easy setup for authentication, API, and database capabilities. This template provides a foundational React application integrated with AWS Amplify, streamlined for scalability and performance. It is ideal for developers looking to jumpstart their project with pre-configured AWS services like Cognito, AppSync, and DynamoDB.
 
 ## Features
 
@@ -20,6 +16,17 @@ This template equips you with a foundational React application integrated with A
 
 For detailed instructions on deploying your application, refer to the [deployment section](https://docs.amplify.aws/react/start/quickstart/#deploy-a-fullstack-app-to-aws) of our documentation.
 
+URL of deployed app (as viewed on `main` branch): https://main.driio4xckehu.amplifyapp.com/
+
+## Local Development (mostly for frontend)
+
+Make sure `node.js` and `npm` are installed. There are many different ways to do this, but one simple tool is `nvm`, see the links for [Windows](https://github.com/coreybutler/nvm-windows) and for [MacOS / Linux](https://github.com/nvm-sh/nvm).
+
+In the root directory of this repo, run `npm install`. 
+
+To relaunch the local dev server, run `npm run dev` (and Ctrl-C to terminate).
+Navigate to the local link that appears in the terminal (i.e. `http://localhost:5173`).
+
 ## Security
 
 See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
@@ -27,4 +34,3 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
->>>>>>> template-repo/main
--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,71 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import Tabs from "./Tabs";
+import "./App.css";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+	useEffect(() => {
+		client.models.Todo.observeQuery().subscribe({
+		next: (data) => setTodos([...data.items]),
+		});
+	}, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+	function createTodo() {
+		client.models.Todo.create({ content: window.prompt("Todo content") });
+	}
 
 	function deleteTodo(id: string) {
 		client.models.Todo.delete({ id })
 	}
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-						onClick={() => deleteTodo(todo.id)}
-						key={todo.id}>{todo.content}
-					</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+	function openTab(evt, tabName) {
+		var i, tabcontent, tablinks;
+		console.log(tabName); // DEBUG
+		tabcontent = document.getElementsByClassName("tabcontent");
+		for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "none";
+		}
+		tablinks = document.getElementsByClassName("tablinks");
+		for (i = 0; i < tablinks.length; i++) {
+			tablinks[i].className = tablinks[i].className.replace(" active", "");
+		}
+		console.log(document.getElementById(tabName))
+		console.log(document.getElementById(tabName).style)
+		console.log(document.getElementById(tabName).style.display)
+		document.getElementById(tabName).style.display = "block";
+		evt.currentTarget.className += " active";
+	}
+
+	return (
+		<main>
+			<img className="logo" src="/src/assets/bch-logo.svg" alt="BCH Logo"></img>
+			<div>
+			  <Tabs>
+				<div label="View Outage Map">
+					<div className="map-container">
+					<iframe src="https://www.openstreetmap.org/export/embed.html?bbox=-123.29681396484376%2C49.194717870320666%2C-123.03726196289064%2C49.320422679265924&amp;layer=mapnik" title="Open Street Maps Placeholder"></iframe>
+					</div>
+				</div>
+				<div label="Report an Outage">
+					<button onClick={createTodo}>+ new</button>
+					<ul>
+						{todos.map((todo) => (
+							<li 
+							onClick={() => deleteTodo(todo.id)}
+							key={todo.id}>{todo.content}
+							</li>
+						))}
+					</ul>
+				</div>
+			  </Tabs>
+			</div>
+		</main>
+	);
 }
 
 export default App;
