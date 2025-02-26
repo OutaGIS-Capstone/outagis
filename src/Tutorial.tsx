4b4c86d1371d0284dc6e581dcca24743a38f4f50
Natalie Balashov <margaret.balashov@gmail.com>
2025-02-26 14:55:21 -0800

    Fix bug in tutorial page

--- src/Tutorial.tsx
+++ src/Tutorial.tsx
@@ -1,19 +1,21 @@
-import Container from '@mui/material';
+import { useNavigate } from "react-router-dom";
+import { Container, Button } from '@mui/material';
 
 function Tutorial() {
+    const navigate = useNavigate();
 	return (
-    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Container maxWidth="xs">
-	    <div>
-		  <p>PLACEHOLDER: this page will contain a tutorial video about how to use outage reporting tool</p>
-		</div>
-		<div>
-		  <a href="/">
-			<p>Navigate back to the main page</p>
-		  </a>
-		</div>
-      </Container>
-    </div>
+	  <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+	    <Container maxWidth="xs">
+		  <div>
+		    <p>PLACEHOLDER: this page will contain a tutorial video about how to use outage reporting tool</p>
+		  </div>
+		  <div>
+		    <Button variant="contained" onClick={() => navigate("/report-outage")}>
+			  Back to the outage reporting page
+			</Button>
+		  </div>
+		</Container>
+	  </div>
 	);
 };
 
