0605b99077abf2212b091ea397e8e109fd801b24
Natalie Balashov <margaret.balashov@gmail.com>
2025-02-26 15:40:09 -0800

    Add fairly hacky way to "redirect" to Home page on sign-in
    Isn't fantastic, need to figure out how to actually navigate to the
    correct page, not just render it. (But it works, so I think fine as
    temporary measure...)

--- src/Signin.tsx
+++ src/Signin.tsx
@@ -3,45 +3,19 @@ import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 import { Box, Button, Paper, Typography } from '@mui/material';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import Home from "./Home.tsx";
 
 Amplify.configure(outputs);
 
 function Signin() {
+  const { authStatus } = useAuthenticator(context => [context.authStatus]);
+
   return (
-    <Box 
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        padding: 2,
-      }}
-    >
-        <Authenticator>
-          {({ signOut, user }) => (
-            <main>
-              <Box sx={{ textAlign: 'center', marginBottom: 2 }}>
-                <Typography variant="h4" color="primary">
-                  Welcome Back!
-                </Typography>
-                <Typography variant="h6" color="textSecondary" sx={{ marginTop: 1 }}>
-                  Hello {user?.userId}, you're logged in!
-                </Typography>
-              </Box>
-              <Box sx={{ textAlign: 'center' }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={signOut}
-                  sx={{ width: '100%', padding: 1.5 }}
-                >
-                  Sign Out
-                </Button>
-              </Box>
-            </main>
-          )}
-        </Authenticator>
-    </Box>
+    <>
+      {authStatus === 'configuring' && 'Loading...'}
+      {authStatus !== 'authenticated' ? <Authenticator /> : <Home />}
+    </>
   );
 }
 
